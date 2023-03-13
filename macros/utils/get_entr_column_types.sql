6cbadd61d63a986bd5c65b47007a8aaf5851c922
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-03-13 01:42:37 -0400

    enable column type overrides

--- macros/utils/get_entr_column_types.sql
+++ macros/utils/get_entr_column_types.sql
@@ -1,4 +1,5 @@
 {% macro get_entr_column_types() %}
+    {% set overrides = var('entr_column_type_overrides', {}) %}
     {% set coltypes = {
         "PLANT_ID": dbt_utils.type_int(),
         "WIND_TURBINE_ID": dbt_utils.type_int(),
@@ -10,5 +11,8 @@
         "VALUE_TYPE": dbt_utils.type_string(),
         "VALUE_UNITS": dbt_utils.type_string(),
     } %}
+    {% for k,v in overrides.items() %}
+        {% do coltypes.update({k:v}) %}
+    {% endfor %}
     {{return(coltypes)}}
 {% endmacro %}
