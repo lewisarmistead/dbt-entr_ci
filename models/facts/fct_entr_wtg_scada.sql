1412db390ff81b5fbe585a81cda7e30819ce902a
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-03-13 01:59:57 -0400

    replicate union pattern from `fct_entr_plant_data`

--- models/facts/fct_entr_wtg_scada.sql
+++ models/facts/fct_entr_wtg_scada.sql
@@ -1,1 +1,13 @@
-select 1 as tmp
+{% set relation_names = var('fct_entr_wtg_scada_models') %}
+{% set relations = [] %}
+{%- for rel in relation_names %}
+    {% do relations.append( ref(rel) ) %}
+{% endfor -%}
+
+
+select * from
+{{dbt_utils.union_relations(
+    relations=relations,
+    include=dbt_utils.get_filtered_columns_in_relation(ref('int_fct_entr_wtg_scada__structured')),
+    column_override=get_entr_column_types()
+)}}
