cb55873bab57543f17f22839100a39ee54090268
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-03-12 21:23:58 -0400

    reincorporate macro for model selection

--- models/facts/fct_entr_plant_data.sql
+++ models/facts/fct_entr_plant_data.sql
@@ -1,2 +1,5 @@
-select 1 as tmp
-{# {{ entr.union_entr_relations(entr_table='fct_entr_plant_data') }} #}
+select * from
+{{entr.union_entr_relations(
+    entr_table='fct_entr_plant_data',
+    column_list=dbt_utils.get_filtered_columns_in_relation(ref('int_fct_entr_plant_data__structured'))
+)}}
