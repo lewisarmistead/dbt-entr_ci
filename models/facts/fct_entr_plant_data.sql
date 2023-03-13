55d5d41f0fc20c6b0ab47864091d61d8149043a5
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-03-12 21:34:06 -0400

    Revert "reincorporate macro for model selection"
    This reverts commit cb55873bab57543f17f22839100a39ee54090268.

--- models/facts/fct_entr_plant_data.sql
+++ models/facts/fct_entr_plant_data.sql
@@ -1,5 +1,2 @@
-select * from
-{{entr.union_entr_relations(
-    entr_table='fct_entr_plant_data',
-    column_list=dbt_utils.get_filtered_columns_in_relation(ref('int_fct_entr_plant_data__structured'))
-)}}
+select 1 as tmp
+{# {{ entr.union_entr_relations(entr_table='fct_entr_plant_data') }} #}
