4ca099507b6b3034e3310ac47a5ac4490105b02d
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-03-13 01:10:50 -0400

    replace column overrides with project var

--- models/facts/fct_entr_plant_data.sql
+++ models/facts/fct_entr_plant_data.sql
@@ -9,15 +9,5 @@ select * from
 {{dbt_utils.union_relations(
     relations=relations,
     include=dbt_utils.get_filtered_columns_in_relation(ref('int_fct_entr_plant_data__structured')),
-    column_override={
-        "PLANT_ID": dbt_utils.type_int(),
-        "WIND_TURBINE_ID": dbt_utils.type_int(),
-        "REANALYSIS_DATASET_ID": dbt_utils.type_int(),
-        "ENTR_TAG_ID": dbt_utils.type_int(),
-        "DATE_TIME": dbt_utils.type_timestamp(),
-        "TAG_VALUE": dbt_utils.type_numeric(),
-        "INTERVAL_S": dbt_utils.type_numeric(),
-        "VALUE_TYPE": dbt_utils.type_string(),
-        "VALUE_UNITS": dbt_utils.type_string(),
-    }
+    column_override=var('entr_column_types')
 )}}
