51506c7242e9473d2a97ff29b2908ab445248352
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-03-12 22:13:02 -0400

    align pattern for handling `execute` condition

--- macros/utils/get_entr_relations.sql
+++ macros/utils/get_entr_relations.sql
@@ -3,15 +3,18 @@
 {% endmacro %}
 
 {% macro default__get_entr_relations(entr_table) %}
+
+    {#-- Prevent querying of db in parsing mode. This works because this macro does not create any new refs. -#}
+    {%- if not execute %}
+        {{ return('') }}
+    {% endif -%}
+
     {% set identified_relations = [] %}
-    {% if execute %}
-        {% for node in graph.nodes.values() | selectattr("resource_type", "in", ["model", "seed"]) %}
-            {%- if node.config.meta.get('entr_table') == entr_table %}
-                {% do identified_relations.append( ref(node.name) ) %}
-            {% endif -%}
-        {% endfor%}
-    {% else %}
-        {% do identified_relations.append( api.Relation.create() ) %}
-    {% endif %}
+    {% for node in graph.nodes.values() | selectattr("resource_type", "in", ["model", "seed"]) %}
+        {%- if node.config.meta.get('entr_table') == entr_table %}
+            {% do identified_relations.append( ref(node.name) ) %}
+        {% endif -%}
+    {% endfor%}
     {{ return(identified_relations) }}
+    
 {% endmacro %}
