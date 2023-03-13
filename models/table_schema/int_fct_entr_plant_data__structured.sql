c685fc930e9ab3c3db929beb448b008408a1c755
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-03-12 21:47:30 -0400

    follow dbt_artifacts pattern for std. table struct

--- models/table_schema/int_fct_entr_plant_data__structured.sql
+++ models/table_schema/int_fct_entr_plant_data__structured.sql
@@ -1,14 +1,14 @@
-with
-    structure as (
-        select
-            cast(0 as {{dbt_utils.type_int()}}) as plant_id,
-            cast(0 as {{dbt_utils.type_int()}}) as entr_tag_id,
-            cast('2000-01-01 00:00:00Z' as {{dbt_utils.type_timestamp()}}) as date_time,
-            cast(0 as {{dbt_utils.type_numeric()}}) as tag_value,
-            cast(0 as {{dbt_utils.type_numeric()}}) as interval_s,
-            cast('str' as {{dbt_utils.type_string()}}) as value_type,
-            cast('str' as {{dbt_utils.type_string()}}) as value_units
-        limit 0
-    )
+with dummy_cte as (
+    select 1 as foo
+)
 
-select * from structure
+select
+    cast(null as {{dbt_utils.type_int()}}) as plant_id,
+    cast(null as {{dbt_utils.type_int()}}) as entr_tag_id,
+    cast(null as {{dbt_utils.type_timestamp()}}) as date_time,
+    cast(null as {{dbt_utils.type_numeric()}}) as tag_value,
+    cast(null as {{dbt_utils.type_numeric()}}) as interval_s,
+    cast(null as {{dbt_utils.type_string()}}) as value_type,
+    cast(null as {{dbt_utils.type_string()}}) as value_units
+from dummy_cte
+where 1=0
