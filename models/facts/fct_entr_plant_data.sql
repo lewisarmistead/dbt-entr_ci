5eb5fbbc12c33eecec0724eaefe6d16d6d5d56b2
lewisarmistead <lewis.armistead@apexcleanenergy.com>
2023-03-12 21:50:22 -0400

    attempt import error by going one macro deeper

--- models/facts/fct_entr_plant_data.sql
+++ models/facts/fct_entr_plant_data.sql
@@ -1,2 +1,16 @@
-select 1 as tmp
-{# {{ entr.union_entr_relations(entr_table='fct_entr_plant_data') }} #}
+select * from
+{{dbt_utils.union_relations(
+        relations=entr.get_entr_relations(entr_table='fct_entr_plant_data'),
+        include=adapter.get_columns_in_relation(ref('int_fct_entr_plant_data__structured'),
+        column_override={
+            "PLANT_ID": dbt_utils.type_int(),
+            "WIND_TURBINE_ID": dbt_utils.type_int(),
+            "REANALYSIS_DATASET_ID": dbt_utils.type_int(),
+            "ENTR_TAG_ID": dbt_utils.type_int(),
+            "DATE_TIME": dbt_utils.type_timestamp(),
+            "TAG_VALUE": dbt_utils.type_numeric(),
+            "INTERVAL_S": dbt_utils.type_numeric(),
+            "VALUE_TYPE": dbt_utils.type_string(),
+            "VALUE_UNITS": dbt_utils.type_string(),
+        }
+)}}
